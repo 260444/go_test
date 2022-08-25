a43e63968afd353f991fa5b7da45ed5a727a6e6f
“260444” <2603485744@qq.com>
2022-08-25 13:21:00 +0800

    orm查询提交

---
+++ orm/查询/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// 定义一个全局对象db
+var db *sql.DB
+
+type user struct {
+	id   int
+	age  int
+	name string
+}
+
+// 定义一个初始化数据库的函数
+func initDB() (err error) {
+	// DSN:Data Source Name
+	dsn := "root:lvshicong990@tcp(127.0.0.1:3306)/sql_test?charset=utf8mb4&parseTime=True"
+	// 不会校验账号密码是否正确
+	// 注意！！！这里不要使用:=，我们是给全局变量赋值，然后在main函数中使用全局变量db
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		return err
+	}
+	// 尝试与数据库建立连接（校验dsn是否正确）
+	err = db.Ping()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func queryRowDemo() {
+	sqlStr := "select id, name, age from sql_test.user where id=?"
+	var u user
+	// 非常重要：确保QueryRow之后调用Scan方法，否则持有的数据库链接不会被释放
+	err := db.QueryRow(sqlStr, 1).Scan(&u.id, &u.name, &u.age)
+	//1就是问好里面的内容
+	if err != nil {
+		fmt.Printf("scan failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("id:%d name:%s age:%d\n", u.id, u.name, u.age)
+}
+
+// 查询多条数据示例
+func queryMultiRowDemo() {
+	sqlStr := "select id, name, age from user where id > ?"
+	rows, err := db.Query(sqlStr, 1)
+	if err != nil {
+		fmt.Printf("query failed, err:%v\n", err)
+		return
+	}
+	// 非常重要：关闭rows释放持有的数据库链接
+	defer rows.Close()
+
+	// 循环读取结果集中的数据
+	for rows.Next() {
+		var u user
+		err := rows.Scan(&u.id, &u.name, &u.age)
+		if err != nil {
+			fmt.Printf("scan failed, err:%v\n", err)
+			return
+		}
+		fmt.Printf("id:%d name:%s age:%d\n", u.id, u.name, u.age)
+	}
+}
+func main() {
+	err := initDB() // 调用输出化数据库的函数
+	if err != nil {
+		fmt.Printf("init db failed,err:%v\n", err)
+		return
+	}
+	queryRowDemo()
+	queryMultiRowDemo()
+}
